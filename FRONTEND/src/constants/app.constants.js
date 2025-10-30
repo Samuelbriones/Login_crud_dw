edf292f2a7817715ffc7fea23a4b097602d391c1
Samuelbriones <samuelbriones558@gmail.com>
2025-10-30 08:18:51 -0500

    add:autenticacion con google

--- FRONTEND/src/constants/app.constants.js
+++ FRONTEND/src/constants/app.constants.js
@@ -1,2 +1,2 @@
-export const GOOGLE_CLIENT_ID = '599032974652-k76repj4vrnrmdc6ap548530g7au0bcl.apps.googleusercontent.com';
+export const GOOGLE_CLIENT_ID = ' datos quemados';
 export const API_BASE_URL = 'http://localhost:8000';
